f4753ff65de1fe5d427a481e3dbd39e639d59f49
shariqazeem <shariqshaukat786@gmail.com>
2024-07-12 22:45:05 +0500

    added staticfiles in settings.py

--- career_development/career_development/settings.py
+++ career_development/career_development/settings.py
@@ -125,9 +125,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Add this line
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
