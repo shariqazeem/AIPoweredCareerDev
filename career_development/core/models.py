8af79657c3f1b9d0fca5fa2f25063d4331cf033d
shariqazeem <shariqshaukat786@gmail.com>
2024-07-21 04:13:27 +0500

    revert changes

--- career_development/core/models.py
+++ career_development/core/models.py
@@ -75,7 +75,7 @@ class Profile(models.Model):
         if self.user.is_superuser:
             return True
 
-        required_fields = ['bio', 'location', 'career_goals']
+        required_fields = ['bio', 'location', 'career_interests', 'skills', 'career_goals', 'professional_experience']
         for field in required_fields:
             if not getattr(self, field):
                 return False
